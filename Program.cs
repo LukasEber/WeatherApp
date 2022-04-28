2fda3b35a7be20d11662fe35eaf8447c3202be1a
Lukas Eber <lukaseber9@gmail.com>
2022-04-28 09:37:59 +0200

    //

--- Program.cs
+++ Program.cs
@@ -6,19 +6,43 @@ namespace WeatherApp
 {
     public class Program
     {
+        private static char newCity = 'y';
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter city: ");
-            Weather.City = Console.ReadLine();
+           
+            while(newCity == 'y')
+            {
+                Console.WriteLine("Enter city: ");
+                Weather.City = Console.ReadLine();
+
+                try
+                {
+                    Weather.GetNewWeatherData();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An unhandled error has occured. Please check your inputs.");
+                }
+                finally
+                {
+                    NewCity();
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static void NewCity()
+        {
+            Console.WriteLine("New city? y/n:");
             try
             {
-                Weather.GetNewWeatherData();
+                newCity = char.Parse(Console.ReadLine());
             }
             catch (Exception ex)
             {
-                Console.WriteLine("An unhandled error has occured. Please check your inputs." + ex.Message);
+                Console.WriteLine("invalid input.");
+                NewCity();
             }
-            Console.ReadKey();
         }
     }
 }
